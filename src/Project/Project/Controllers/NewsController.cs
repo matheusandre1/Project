fedb8a3368ead8b5a4fa8451667315bd553e4f07
Matheus Andre <matheusandr2@gmail.com>
2024-09-04 00:00:04 -0300

    feat: create project

---
+++ src/Project/Project/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly ILogger<NewsController> _logger;
+        public NewsController(ILogger<NewsController> logger)
+        {
+            _logger = logger;
+            
+        }
+    }
+}
