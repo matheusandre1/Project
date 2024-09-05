b4f60b0d25bfeb10524b80cc00fa1fd27ddb7a37
Matheus Andre <matheusandr2@gmail.com>
2024-09-04 23:35:44 -0300

    feat: new changes

---
+++ src/Project.Api/Entities/News.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Project.Api.Entities
+{
+    public class News : BaseEntity
+    {
+        public News(string hat, string title, string text, string author, string img, string link, DateTime publishDate, bool active)
+        {
+            Hat = hat;
+            Title = title;
+            Text = text;
+            Author = author;
+            Img = img;
+            Link = link;
+            PublishDate = DateTime.Now;
+            Active = active;
+        }
+
+        [BsonElement("hat")]
+        public string Hat { get; private set; } = string.Empty;
+
+        [BsonElement("title")]
+        public string Title { get; private set; } = string.Empty;
+
+        [BsonElement("text")]
+        public string Text { get; private set; } = string.Empty;
+
+        [BsonElement("author")]
+        public string Author { get; private set; } = string.Empty;
+
+        [BsonElement("img")]
+        public string Img { get; private set; } = string.Empty;
+
+        [BsonElement("link")]
+        public string Link { get; private set; } = string.Empty;
+
+        [BsonElement("publishDate")]
+        public DateTime PublishDate { get; private set; }
+
+        [BsonElement("active")]
+        public bool Active { get; private set; }
+    }
+
+}
