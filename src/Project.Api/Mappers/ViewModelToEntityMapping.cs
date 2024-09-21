c2e708dcbaa1e433d907beb4c85dffed9d4319fa
Matheus Andre <matheusandr2@gmail.com>
2024-09-21 12:55:27 -0300

    feat: new changes AutoMapper

---
+++ src/Project.Api/Mappers/ViewModelToEntityMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Project.Api.Entities;
+using Project.Api.Entities.ViewModels;
+
+namespace Project.Api.Mappers
+{
+    public class ViewModelToEntityMapping : Profile
+    {
+        public ViewModelToEntityMapping()
+        {
+
+            CreateMap<NewsViewModel, News>();
+        }
+    }
+}
